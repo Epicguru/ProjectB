c9e16f789cbe5534f5fa98bcfff459e95443758b
James Billy <james@epicguru.co.uk>
2019-09-19 20:11:33 +0200

    Cleaning up.

--- Assets/Scripts/Vehicles/VehicleMountedWeapons.cs
+++ Assets/Scripts/Vehicles/VehicleMountedWeapons.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class VehicleMountedWeapons : MonoBehaviour
 {
-    public MountedWeapon Weapon;
     public MountedWeaponSpot[] Spots;
 
     private void Awake()
@@ -15,14 +14,6 @@ public class VehicleMountedWeapons : MonoBehaviour
             var spot = Spots[i];
             spot.Vehicle = this;
             spot.SetNodeID((byte)i);
-        }
-
-        if (JNet.IsServer)
-        {
-            var spawned = Instantiate(Weapon);
-            JNet.Spawn(spawned.gameObject);
-
-            Spots[0].SetMountedWeapon(spawned);
         }
     }
 
